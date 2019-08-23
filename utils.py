b75c7cb9c56eb01f6130ee3466e123fc6f8414c1
Taylan Bilal <taylanbil@google.com>
2019-08-23 09:44:01 -0700

    Some cleanup of default values and utils (#10)

--- utils.py
+++ utils.py
@@ -13,3 +13,6 @@ def initialize_path(*deps):
     dep_folder = os.path.join(root_folder, 'deps', dep)
     sys.path.insert(0, dep_folder)
 
+
+def now():
+  return datetime.now().strftime('%H:%M:%S')
