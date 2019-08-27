74619a320edc085e786c3f15ff2ae518e83a03de
Taylan Bilal <taylanbil@google.com>
2019-08-27 16:23:16 -0700

    pyformat

--- fairseq_train_tpu.py
+++ fairseq_train_tpu.py
@@ -186,8 +186,7 @@ def parse_args():
 
 def parse_input_shapes(input_shapes_arg):
   input_shapes = (
-      shape.replace('*', 'x').split('x')
-      for shape in input_shapes_arg)
+      shape.replace('*', 'x').split('x') for shape in input_shapes_arg)
   input_shapes = [list(map(int, shape)) for shape in input_shapes]
   input_shapes.sort(key=lambda shape: shape[1])
   return input_shapes
@@ -241,7 +240,8 @@ def main_tpu(args):
       rates = tracker.rate(), tracker.global_rate()
       msg += ', Rate={:.2f}, Global Rate={:.2f}'.format(*rates)
     if log_output:
-        msg += 'loss={:.4f}, nll_loss={:.4f}'.format(log_output['loss'].item(), log_output['nll_loss'].item())
+      msg += 'loss={:.4f}, nll_loss={:.4f}'.format(
+          log_output['loss'].item(), log_output['nll_loss'].item())
     return msg
 
   def train_loop_fn(model, loader, device, context):
@@ -250,7 +250,9 @@ def main_tpu(args):
     tracker = xm.RateTracker()
     for i, samples in loader:
       if i and not (i % args.log_steps):
-        print(log_step('training', device, i, log_output=log_output, tracker=tracker))
+        print(
+            log_step(
+                'training', device, i, log_output=log_output, tracker=tracker))
       log_output = trainer.train_step(samples)
       xm.optimizer_step(trainer.optimizer)
       tracker.add(sum(sample['nsentences'] for sample in samples))
