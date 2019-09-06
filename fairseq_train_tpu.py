5da31fcdb8719b76969310281ea58fc5d82e537d
Taylan Bilal <taylanbil@google.com>
2019-09-06 22:23:43 +0000

    removed space in global rate

--- fairseq_train_tpu.py
+++ fairseq_train_tpu.py
@@ -271,7 +271,7 @@ def main_tpu(args):
                                               device, step)
     if tracker:
       rates = tracker.rate(), tracker.global_rate()
-      msg += ', Rate={:.2f}, Global Rate={:.2f}'.format(*rates)
+      msg += ', Rate={:.2f}, GlobalRate={:.2f}'.format(*rates)
     if log_output:
       msg += ', loss={:.4f}, nll_loss={:.4f}'.format(
           log_output['loss'].item(), log_output['nll_loss'].item())
